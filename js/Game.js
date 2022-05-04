7828ba9f92a91a6e228d9fc70b910979c8c428e4
GFX23 <Spacil-Tomas@seznam.cz>
2022-05-04 20:23:41 +0200

    Updated v1

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,59 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0
+        this.phrases = [new Phrase("You Lose"), new Phrase("You Win"), new Phrase("Play Again"),
+                        new Phrase("Game Over"), new Phrase("Good Luck")] 
+        this.activePhrase = null
+    }
+
+    startGame() {
+        document.querySelector("#overlay").style.display = "none"
+        this.activePhrase = this.getRandomPhrase()
+        this.activePhrase.addPhraseToDisplay()
+    }
+
+    getRandomPhrase() {
+        const randomNum = Math.floor(Math.random()) * this.phrases.length
+        return phrases[randomNum]
+    }
+
+    handleInteraction() {
+        const keys = document.querySelectorAll(".key")
+        keys.forEach(key => {
+            key.addEventListener("click", e => {
+                e.target.addAtributte("disabled", true)
+                if (!this.activePhrase.checkLetter(e.target.innerHTML)) {
+                    e.target.classList.add("wrong")
+                    this.removeLife()
+                } else {
+                    e.target.classList.add("chosen")
+                    this.activePhrase.showMatchedLetter(e.target.innerHTML)
+                    if(this.checkForWin()) {
+                        this.gameOver()
+                    }
+                }
+            })
+        })
+    }
+
+    removeLife() {
+        const tries = document.querySelectorAll(".tries")
+        tries[this.missed].setAttribute("src", "images/lostHeart.png")
+        this.missed += 1
+        if (this.missed === 5) {
+            this.gameOver()
+        }
+    }
+
+    checkForWin() {
+        return document.querySelectorAll(".letter hide").length === 0
+    }
+
+    gameOver() {
+        document.querySelector("#overlay").style.display = "none"
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,40 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase
+    }
+
+    addPhraseToDisplay() {
+        const ul = document.querySelector("ul")
+        for (let i = 0; i < this.phrase.length; i++) {
+            if (this.phrase[i] === " ") {
+                const li = document.createElement("li")
+                li.innerHTML = " "
+                li.classList.add("space")
+                ul.appendChild(li)
+            } else {
+                const li = document.createElement("li")
+                li.innerHTML = `${this.phrase[i]}`
+                li.classList.add(`hide letter ${this.phrase[i]}`)
+                ul.appendChild(li)
+            }
+        }
+    }
+
+    checkLetter(letter) {
+        return this.phrase.toLowerCase().includes(letter)
+    }
+
+    showMatchedLetter(letter) {
+        const phraseLetters = document.querySelectorAll(".letter")
+        phraseLetters.forEach(pLetter => {
+            if (this.checkLetter(letter)) {
+                pLetter.classList.remove("hide")
+                pLetter.classList.add("show")
+            }
+        })
+    }
+}
